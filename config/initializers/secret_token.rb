59b808f0d60edc4d0334ed98c5d1439a85d7fe63
Waley Chen <nikeelevet@hotmail.com>
2012-02-20 02:17:11 -0500

    fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vekii::Application.config.secret_token = '5d4b0d6a52059bc97f81aec96fc0460645752609ec0e74c0263c3c927739e91ee676a0a43f394742ce42b75800902697ded7519450875c7de0efb0d8c48e0af2'
