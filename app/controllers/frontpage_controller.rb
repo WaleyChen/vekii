73093e5d652f60d398ec3bdda86bb84bcb827e51
Waley Chen <nikeelevet@hotmail.com>
2012-03-21 03:17:51 -0400

    changes for heroku deployment

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -7,13 +7,15 @@ class FrontpageController < ApplicationController
   def frontpage
     devKey = "AI39si5Cwgvp6TJAY4pqrUcK8dCcL8WntrOGNmmn6MBvBpN40Ru_pKF99Y0m-y_WJvLxtblt4REVaTqlQYsmr5Q05E1Bwvkmyw"
     @googleLoginLink = "https://accounts.google.com/o/oauth2/auth?"
+    client_id = "client_id=908038792880-vm3862hmpnp7u6gnmgd8104g8u7r1sr1.apps.googleusercontent.com"
+    gscope = "scope=https://gdata.youtube.com"
+    response_type = "response_type=token"
     
     # construct the link to request an access token from the Youtube API
     if ENV["RAILS_ENV"] = "test"
-        client_id = "client_id=908038792880-vm3862hmpnp7u6gnmgd8104g8u7r1sr1.apps.googleusercontent.com"
-        redirect_uri = "redirect_uri=http://localhost:3000/oauth2callback"
-        gscope = "scope=https://gdata.youtube.com"
-        response_type = "response_type=token"
+      redirect_uri = "redirect_uri=http://localhost:3000/oauth2callback"
+    elsif ENV["RAILS_ENV"] = "production"
+      redirect_uri = "redirect_uri=http://vekii.com/oauth2callback"
     end
     
     @googleLoginLink = @googleLoginLink + client_id + '&' + redirect_uri + '&' + gscope + '&' + response_type
