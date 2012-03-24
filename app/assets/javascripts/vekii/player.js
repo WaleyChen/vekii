467d1c281aaa73350daeaffd09574307b573b0e3
Waley Chen <nikeelevet@hotmail.com>
2012-03-23 22:17:56 -0400

    playing with the front end

--- app/assets/javascripts/vekii/player.js
+++ app/assets/javascripts/vekii/player.js
@@ -14,7 +14,7 @@ function delete_Song(playlist_title, song_title) {
 			jQuery.each(playlist.songs, function(song_index, song) {
 				if (song.title == song_title) {
 					playlists.playlists[playlist_index].songs.splice(song_index, 1);
-					show_Playlist(playlist_title);
+					show_Playlists(playlist_title);
 					return false;
 				}
 			});
@@ -35,63 +35,73 @@ function playlists_Sort_Func(a, b) {
 	}
 }
 
-function show_Playlist(title) {
-	$("#playlist").html('');
-	$("#playlist").append("Playlist - " + title + '</br>');
+function show_Playlists(playlist_to_show) {
+	$("#playlists").html('');
 	
 	jQuery.each(playlists.playlists, function(index, playlist) {
-		if (playlist.title == title) { 
+		$("#playlists").append("<li><a href=\"javascript:show_Playlists('" 
+							   + playlist.title 
+							   + "')\">"
+							   + playlist.title 
+							   + "</a>");
+		$("#playlists").append("<a href=\"javascript:delete_Playlist(\'"
+								+ playlist.title 
+								+ "\')\" >"
+									+ "<img src=\"/assets/delete.png\""
+									+ "align=\"right\""
+									+ "alt=\"delete.png\""
+									+ "height=\"10\"" 
+									+ "width=\"10\""
+									+ "/>"  
+								+ "</a></li>");
+
+		/*
+								<div class="well" style="padding: 8px 0;">
+								        <ul class="nav nav-list">
+								          <li class="active"><a href="#"><i class="icon-home icon-white"></i> Home</a></li>
+								          <li><a href="#"><i class="icon-book"></i> Library</a></li>
+								          <li><a href="#"><i class="icon-pencil"></i> Applications</a></li>
+								          <li><a href="#">Misc</a></li>
+								        </ul>
+								      </div> <!-- /well -->
+		*/
+		playlist.songs.sort(playlists_Sort_Func);
+		
+		if (playlist.title == playlist_to_show) { 
 			jQuery.each(playlist.songs, function(index, song) {
 				// for the delete_Song function, make the string parameter friendly
 				// http://stackoverflow.com/questions/1873/triple-quotes-how-do-i-delimit-a-databound-javascript-string-parameter-in-asp-n
 				song_title_as_param = song.title.replace(/\'/g, "\\\'");
 				song_title_as_param = song_title_as_param.replace(/\"/g, "&#34;");
 				
+				if (index == 0) {
+					$("#playlists").append("<ul id=\"playlist\" class=\"nav nav-list\">");
+					$("#playlists").append("</ul>");
+				}
+				
+				$("#playlist").append("<li>"
+										+ "<a href=\"javascript:ytplayer.loadVideoById('" 
+									  	+ song.video_id 
+									  	+ "')\">" 
+										+ song.title 
+										+ " "
+										+ "</a>");
 				$("#playlist").append("<a href=\"javascript:delete_Song(\'"
 										+ playlist.title 
 										+ "\', \'"
 										+ song_title_as_param
 										+ "\')\" >"
 											+ "<img src=\"/assets/delete.png\""
+											+ "align=\"right\""
 											+ "alt=\"delete.png\""
 											+ "height=\"10\"" 
 											+ "width=\"10\""
 											+ "/>"  
-										+ "</a>");
-				$("#playlist").append(" <a href=\"javascript:ytplayer.loadVideoById('" 
-									  	+ song.video_id 
-									  	+ "')\">" 
-										+ song.title 
-										+ " "
-										+ "</a>" 
-										+ "</br>");
+										+ "</a>"
+										+ "</li>");
 			});
 		}
-	});
-}
-
-function show_Playlists() {
-	$("#playlists").html('');
-	$("#playlists").append("Playlists</br>");
-	
-	jQuery.each(playlists.playlists, function(index, playlist) {
-		$("#playlists").append("<a href=\"javascript:delete_Playlist(\'"
-								+ playlist.title 
-								+ "\')\" >"
-									+ "<img src=\"/assets/delete.png\""
-									+ "alt=\"delete.png\""
-									+ "height=\"10\"" 
-									+ "width=\"10\""
-									+ "/>"  
-								+ "</a>");
-		
-		$("#playlists").append(" <a href=\"javascript:show_Playlist('" 
-							   + playlist.title 
-							   + "')\">"
-							   + playlist.title 
-							   + "</a> </br>");
-
-		playlist.songs.sort(playlists_Sort_Func);										
+												
 		playlists_JSON = JSON.stringify(playlists);
 	});
 }
